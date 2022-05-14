a52682ff9e5077384da3305195aa31f0f4e66926
Reza Jax <rezajaxg@gmail.com>
2022-05-14 16:28:59 +0430

    mutableStateOf and remember test

---
+++ app/src/main/java/app/bidar/tiptime/MainActivity.kt
@@ -0,0 +1,81 @@
+package app.bidar.tiptime
+
+import android.annotation.SuppressLint
+import android.graphics.Paint
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.bidar.tiptime.ui.theme.TipTimeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TipTimeScreen()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(Modifier.height(16.dp))
+        EditNumberField()
+
+    }
+}
+
+@Composable
+fun EditNumberField() {
+
+    var amountInput = mutableStateOf("0")
+    Log.i("rez", "amountInput.value")
+    TextField(
+        value = amountInput.value,
+        onValueChange = { amountInput.value = it
+            Log.i("rez", it)
+                        },
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TipTimeTheme {
+        TipTimeScreen()
+    }
+}
