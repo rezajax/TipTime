52657ba66c979ffa2f3159f7b357e8f52f3e1a9e
rezInHome <rezajaxg@gmail.com>
2022-05-15 00:22:16 +0430

    commit in home in night

--- app/src/main/java/app/bidar/tiptime/MainActivity.kt
+++ app/src/main/java/app/bidar/tiptime/MainActivity.kt
@@ -11,9 +11,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -58,16 +56,18 @@ fun TipTimeScreen() {
     }
 }
 
+
+
+/**
+ * Remember the value produced by [calculation]. [calculation] will only be evaluated during the composition.
+ * Recomposition will always return the value produced by composition.
+ */
 @Composable
 fun EditNumberField() {
-
-    var amountInput = mutableStateOf("0")
-    Log.i("rez", "amountInput.value")
+    var reComposable by remember { mutableStateOf("0") }
     TextField(
-        value = amountInput.value,
-        onValueChange = { amountInput.value = it
-            Log.i("rez", it)
-                        },
+        value = reComposable,
+        onValueChange = { reComposable = it },
     )
 }
 
